936ea439c1ab8ac9a293a2519822353622d65eae
schwendh <proplaner@gmx.net>
2021-12-29 15:04:47 +0100

    41. Using the specification methods in the controller

--- Core/Specifications/ProductsWithTypesAndBrandsSpecification.cs
+++ Core/Specifications/ProductsWithTypesAndBrandsSpecification.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Threading.Tasks;
 using Core.Entities;
 
@@ -12,6 +13,12 @@ namespace Core.Specifications
         {
             AddInclude(p => p.ProductType);
             AddInclude(p => p.ProductBrand);
+        }
+
+        public ProductsWithTypesAndBrandsSpecification(int id) : base(x => x.Id == id)
+        {
+            AddInclude(p => p.ProductType);
+            AddInclude(p => p.ProductBrand);            
         }
     }
 }
