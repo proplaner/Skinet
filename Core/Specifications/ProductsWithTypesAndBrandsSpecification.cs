32c7a437b24a6df9ba8e28e45e7cec9240689de5
schwendh <proplaner@gmx.net>
2021-12-31 15:25:38 +0100

    67. Adding the search functionality

--- Core/Specifications/ProductsWithTypesAndBrandsSpecification.cs
+++ Core/Specifications/ProductsWithTypesAndBrandsSpecification.cs
@@ -10,7 +10,9 @@ namespace Core.Specifications
     public class ProductsWithTypesAndBrandsSpecification : BaseSpecification<Product>
     {
         public ProductsWithTypesAndBrandsSpecification(ProductSpecParams productParams) 
-            : base(x => 
+            : base(x =>
+                (string.IsNullOrEmpty(productParams.Search) || x.Name.ToLower().Contains
+                (productParams.Search)) &&  
                 (!productParams.BrandId.HasValue || x.ProductBrandId == productParams.BrandId) &&
                 (!productParams.TypeId.HasValue || x.ProductTypeId == productParams.TypeId)
             )
