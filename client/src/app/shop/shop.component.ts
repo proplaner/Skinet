e8fedc0af1685949786ad070101385ce3342f0e1
schwendh <proplaner@gmx.net>
2022-01-02 09:41:28 +0100

    100. Adding the pagination functionality part 3

--- client/src/app/shop/shop.component.ts
+++ client/src/app/shop/shop.component.ts
@@ -71,4 +71,9 @@ export class ShopComponent implements OnInit {
     this.shopParams.sort = sort;
     this.getProducts();
   }
+
+  onPageChanged(event: any){
+    this.shopParams.pageNumber = event.page;
+    this.getProducts();
+  }
 }
