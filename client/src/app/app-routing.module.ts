f72672525947e6a238a87835ef19c9124493e550
schwendh <proplaner@gmx.net>
2022-01-03 14:10:40 +0100

    127. Using the breadcrumb service

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,8 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { ProductDetailsComponent } from './shop/product-details/product-details.component';
-import { ShopComponent } from './shop/shop.component';
 import {TestErrorComponent} from "./core/test-error/test-error.component";
 import {ServerErrorComponent} from "./core/server-error/server-error.component";
 import {NotFoundComponent} from "./core/not-found/not-found.component";
