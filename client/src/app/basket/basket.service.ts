80a058062bc5d240a9fb713a54ed0b1702ec5eac
schwendh <proplaner@gmx.net>
2022-01-04 16:08:28 +0100

    150. Adding an item to the basket service method

--- client/src/app/basket/basket.service.ts
+++ client/src/app/basket/basket.service.ts
@@ -2,8 +2,9 @@ import { Injectable } from '@angular/core';
 import {environment} from "../../environments/environment";
 import {HttpClient} from "@angular/common/http";
 import {BehaviorSubject} from "rxjs";
-import {IBasket} from "../shared/models/basket";
+import {Basket, IBasket, IBasketItem} from "../shared/models/basket";
 import {map} from "rxjs/operators";
+import {IProduct} from "../shared/models/product";
 
 @Injectable({
   providedIn: 'root'
@@ -34,5 +35,41 @@ export class BasketService {
 
   getCurrentBasketValue() {
     return this.basketSource.value;
+  }
+
+  addItemToBasket(item: IProduct, quantity = 1){
+    const itemToAdd: IBasketItem = this.mapProductItemToBasketItem(item, quantity);
+    const basket = this.getCurrentBasketValue() ?? this.createBasket();
+    basket.items = this.addOrUpdateItem(basket.items, itemToAdd, quantity);
+    this.setBasket(basket);
+  }
+
+  private addOrUpdateItem(items: IBasketItem[], itemToAdd: IBasketItem, quantity: number): IBasketItem[] {
+        const index = items.findIndex(i => i.id === itemToAdd.id);
+        if (index === -1){
+          itemToAdd.quantity = quantity;
+          items.push(itemToAdd);
+        } else {
+          items[index].quantity += quantity;
+        }
+        return items;
+    }
+
+  private createBasket(): IBasket {
+    const basket = new Basket();
+    localStorage.setItem('basket_id', basket.id);
+    return basket;
+  }
+
+  private mapProductItemToBasketItem(item: IProduct, quantity: number): IBasketItem {
+    return {
+      id: item.id,
+      productName: item.name,
+      price: item.price,
+      pictureUrl: item.pictureUrl,
+      quantity,
+      brand: item.productBrand,
+      type: item.productType
+    };
   }
 }
